91fe7200fd8ac21c93c68f18d85cb64230fb8e36
silvadavitor <silvadavitor999@gmail.com>
2026-03-10 09:23:48 -0300

    chore: setup monorepo structure for problem 1

---
+++ Problema1/src/Program.cs
@@ -0,0 +1,10 @@
+namespace Livraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Problema1/tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Livraria.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
